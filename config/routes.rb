3b22a00e5e255e5b242f6ec9a4bf09c5735ac007
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-09 15:29:30 -0700

    adding a custom action to link

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
 
 # declaring the route on the left, and mapping it on the right
