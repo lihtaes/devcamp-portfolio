38f7e92ca84190e73efc79725ebb7fb5e4d4626d
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-09 13:41:01 -0700

    customize portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   
 # resources pre-configured with generation of respective scaffolds
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
 
 
 # declaring the route on the left, and mapping it on the right
   get '/about-me', to: 'pages#about' 
   get '/contact', to: 'pages#contact'
-
 
   root to: 'pages#home'
 
