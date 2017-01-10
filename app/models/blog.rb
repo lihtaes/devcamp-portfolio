43577f2e6cb6ace8316ad1b636df935c75387d40
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-10 11:51:38 -0700

    integrated database relationship between topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -20,5 +20,8 @@ class Blog < ApplicationRecord
 #The following method ensures that the selected data is present when creating a new database entry for respective model
   validates_presence_of :title, :body
 
+#the blog model needs to know that it belongs to a specific topic with the 
+# following method
+  belongs_to :topic
 
 end
---
+++ app/models/topic.rb
@@ -0,0 +1,7 @@
+class Topic < ApplicationRecord
+
+  validates_presence_of :title
+
+  has_many :blogs
+
+end
