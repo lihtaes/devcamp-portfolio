816a8000481792a082f99053a9fc9ad1fb1301bd
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-10 10:45:56 -0700

    implemented validation for each features data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,5 @@
 class Portfolio < ApplicationRecord
+#The following method ensures that the selected data is present when creating a new database entry for respective model
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+#The following method ensures that the selected data is present when creating a new database entry for respective model
+  validates_presence_of :title, :percent_utilized
 end
