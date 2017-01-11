4b7b76d9ccee4f64468c711f418f54087ea77ff5
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-11 10:51:41 -0700

    added basic authentication with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
 # resources pre-configured with generation of respective scaffolds
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
