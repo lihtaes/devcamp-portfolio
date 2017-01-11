b0576e2f0601153d3f038538eb8c8db957ec61f7
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-11 14:03:17 -0700

    refactored code, created concern for DeviseWhitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #include DeviseWhiteList
+  include DeviseWhitelist
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
