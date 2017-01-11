bc16eafef8680f7c39d627581d94b1517db6ffd2
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-11 10:18:10 -0700

    session stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #include DeviseWhiteList
+
+
+
+#Working with session variables, only when someone comes from target links
+#**********
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
+#**************
+
+
 end
