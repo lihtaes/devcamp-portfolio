c5139f77052b34716b4b48bbf3adbd2669f3945e
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-11 11:55:20 -0700

    added new parameters for user registration and editing account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #include DeviseWhiteList
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
 #Working with session variables, only when someone comes from target links
 #**********
