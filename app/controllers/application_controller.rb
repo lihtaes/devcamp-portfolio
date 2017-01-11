5bf5c2ad65eca4656c89381f5b8f9f7af6634deb
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-11 16:05:03 -0700

    app controller, slight edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
 
-#Working with session variables, only when someone comes from target links
+#Working with session variables, only when someone comes from target links, the output for which
+#can be seen in the application.html.erb file which is located in views/layouts/HERE
 #**********
   before_action :set_source
 
