47c59e6edc0a5ab50c19ae2585cae1b7298ea1d2
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-13 11:03:01 -0700

    merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  include SetSource
+#include CurrentUserConcern
+  include DefaultPageContent
 
 
 
-#Working with session variables, only when someone comes from target links, the output for which
-#can be seen in the application.html.erb file which is located in views/layouts/HERE
-#**********
-  before_action :set_source
 
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
-#**************
 
 
   def current_user
-    super
+    super || OpenStruct.new(name:"Guest User", first_name: "Guest", last_name: "User", email:"guest@example.com")
   end
 
 end
