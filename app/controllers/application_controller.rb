c29b36eebc5ab6692a9128abb2fce50c9f5ef41b
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-13 11:10:43 -0700

    clear cache, update .gitignore, implement OpenStruct for Guest user, refactor accordingly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-#include CurrentUserConcern
+  include CurrentUserConcern
   include DefaultPageContent
-
-
-  def current_user
-    super || OpenStruct.new(name:"Guest User", first_name: "Guest", last_name: "User", email:"guest@example.com")
-  end
 
 end
--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,2 +1,8 @@
 module CurrentUserConcern 
   extend ActiveSupport::Concern
+
+  def current_user
+    super || OpenStruct.new(name:"Guest User", first_name: "Guest", last_name: "User", email:"guest@example.com")
+  end
+
+end
