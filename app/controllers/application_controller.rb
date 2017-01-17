a18b1a95139493c170cab7edcad0b6ca3238cbe3
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-17 10:54:37 -0700

    campbell view tool gem initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,18 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+  def set_copyright
+    @copyright = CampbellViewTool::Renderer.copyright 'Bradley Campbell', 'All rights reserved'
+  end
+
+end
+
+
+module CampbellViewTool
+  class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+  end
 end
