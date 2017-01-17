3fbc942442da4dbfc51403bff9734bb707df37c9
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-17 10:02:45 -0700

    implemented bootstrap gem

--- Gemfile
+++ Gemfile
@@ -56,3 +56,41 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 
 gem 'devise', '~> 4.2'
+
+gem 'bootstrap', '~> 4.0.0.alpha6'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,8 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, 
+                                            :update, 
+                                            :show, 
+                                            :destroy]
   layout "portfolio"
 
   def index
