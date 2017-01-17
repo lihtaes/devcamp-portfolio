4e485a58f68647ca8294dfe02c9cbebc4b54a749
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-17 10:55:43 -0700

    campbell view tool gem initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,3 @@ class ApplicationController < ActionController::Base
 
 end
 
-
-module CampbellViewTool
-  class Renderer
-      def self.copyright name, msg
-        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-      end
-  end
-end
