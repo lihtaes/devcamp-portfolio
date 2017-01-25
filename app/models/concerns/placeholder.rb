a3d2c25c12800bf95b1d03ac5c5f70de384d5f9a
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-24 21:33:48 -0700

    refactored image placeholder for carrier and implemented image uploading for portfolios

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,10 +1,4 @@
 module Placeholder
   extend ActiveSupport::Concern
 
-  def self.image_generator(height:, width:)
-
-    "https://placehold.it/#{height}x#{width}"
-    
-  end
-
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,9 +5,13 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies, 
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  include Placeholder
+
   #The following method ensures that certain data is present when creating a new database entry for respective model
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
+
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
   # **SCOPE**
   #Defining scope for a model can be done in two ways, the first is using the self method referring to the instance of the class, as in the below two examples:
@@ -28,13 +32,4 @@ class Portfolio < ApplicationRecord
 
   #See portfolio_controller
   scope :ruby, -> {where(subtitle: 'Ruby on Rails')}
-
-  #initialization takes place when the NEW method is called in the controller
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '300')
-  end
-
 end
