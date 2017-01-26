50da6fc8944325a8a5d37662ff1a61820dee9a32
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-26 10:30:39 -0700

    twitter gem and functionality

--- Gemfile
+++ Gemfile
@@ -44,7 +44,7 @@ gem 'carrierwave-aws', '~> 1.0', '>= 1.0.2'
 gem 'dotenv-rails', '~> 2.1', '>= 2.1.2'
 gem 'cocoon', '~> 1.2', '>= 1.2.9'
 gem 'gritter', '~> 1.2'
-
+gem 'twitter', '~> 6.1'
 
 
 
--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,6 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
-
-    #config.action_controller.permit_all_parameters = true
-      
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
