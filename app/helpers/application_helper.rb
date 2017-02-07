b3d803737ca688443c93f35076f30a18751883c2
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-02-07 10:37:00 -0700

    source tracker on blog page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,11 +14,11 @@ module ApplicationHelper
     end 
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     # Here is the follow up for the session variable code from the set_source concern in controllers/concerns
      if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{link_to "contact me", contact_path} if you'd like to work together!"
+      content_tag(:div, greeting.html_safe, class: styles)
      end 
   end
 
